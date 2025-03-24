37a791a195c63c85a494a97fd39d30f2512f80e8
Kueiju <73866494+Kueiju@users.noreply.github.com>
2025-03-24 22:18:27 +0800

    Time: 0 ms (100%), Space: 8.2 MB (13.1%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.cpp
+++ 1323-maximum-69-number/1323-maximum-69-number.cpp
@@ -8,11 +8,9 @@ public:
         while(num)
         {
             vec.push_back(num % 10);
-            //cout << vec.back() << endl;
             num /= 10;
         }
         
-        int idx = 0;
         int i = vec.size() - 1;
         for(i; i >= 0; i--)
         {
@@ -24,18 +22,6 @@ public:
         }
 
         ans += 3 * pow(10, i);
-
-        //reverse(vec.begin(), vec.end());
-        
-        // for(int i = vec.size() - 1; i >= 0; i--)
-        // {
-        //     ans += vec[i] * pow(10, i);
-        // }
-
-        // for(int i = 0; i < vec.size(); i++)
-        // {
-        //     ans = ans * 10 + vec[i];
-        // }
         
         return ans;
     }
