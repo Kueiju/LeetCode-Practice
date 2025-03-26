5e43b225b2dcad2fd5d9816e72172ffd47d1925e
Kueiju <73866494+Kueiju@users.noreply.github.com>
2025-03-26 08:34:49 +0800

    Time: 91 ms (36.37%), Space: 84.8 MB (33.59%) - LeetHub

---
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int minSetSize(vector<int>& arr) {
+        
+        unordered_map<int, int> hMap;
+        
+        for(auto val : arr)
+        {
+            hMap[val]++; //O(n)
+        }
+        
+        int origArrSize = arr.size();
+        int n = origArrSize;
+        
+        priority_queue<pair<int, int>> pq;
+        
+        for(auto [key, freq] : hMap)
+        {
+            pq.push({freq, key}); //O(mlogm)
+        }
+        
+        int ans = 0;
+        while(!pq.empty()) //O(n)
+        {
+            int mostFreq = pq.top().first;
+            n -= mostFreq;
+            if( n <= origArrSize/2)
+            {
+                ans++;
+                return ans; 
+            }
+            
+            pq.pop();
+            ans++;
+            
+        }
+        
+        return ans;
+    }
+};
+
+//time complexity: O(mlogm), max heap dominated, each push costs O(logm), m <= n, n: array size
+//space complexity: O(n), unordered_map requires space and dominated, O(m), max heap requires space
