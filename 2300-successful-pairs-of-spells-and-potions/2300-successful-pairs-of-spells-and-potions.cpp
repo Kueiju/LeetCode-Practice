d2136740f3f8109328c2126ccd4d8c1c97f6b28e
Kueiju <73866494+Kueiju@users.noreply.github.com>
2025-04-01 08:44:00 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
+
+        sort(potions.begin(), potions.end());
+        cout << potions[0] << endl;
+
+        vector<int> returnVec;
+        
+        for(auto val : spells)
+        {
+            int sum = 0;
+            int left = 0;
+            int right = potions.size() - 1;
+
+            while(left <= right)
+            {
+                int mid = left + (right - left) / 2;
+                int retval = val * potions[mid];
+                if(retval >= success)
+                {
+                    sum += right - mid + 1;
+                    //cout << "right = " << right << endl;
+                    //cout << "mid = " << mid << endl;
+                    //cout << "sum = " << sum << endl;
+                    right = mid - 1;
+                }
+                else
+                {
+                    left = mid + 1;
+                    //cout << "left = " << left << endl;
+                }
+            }
+
+            returnVec.push_back(sum);
+        }
+        
+        return returnVec;
+    }
+};
