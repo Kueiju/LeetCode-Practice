9c81fe078f705cbd6c245e78dc2871456787e21c
Kueiju <73866494+Kueiju@users.noreply.github.com>
2025-04-02 07:52:56 +0800

    Time: 39 ms (60.7%), Space: 128.4 MB (8.73%) - LeetHub

--- 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.cpp
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.cpp
@@ -3,21 +3,20 @@ public:
     vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
 
         sort(potions.begin(), potions.end());
-        //cout << potions[0] << endl;
 
-        vector<int> returnVec;
-        
-        for(auto val : spells)
+        vector<int> returnArr;
+       
+        for(double spell : spells)
         {
             int left = 0;
-            int right = potions.size() - 1;
-
-            while(left <= right)
+            int right = potions.size();
+            while(left < right)
             {
                 int mid = left + (right - left) / 2;
-                if(potions[mid] >= (success + val - 1) / val)
+                double temp = potions[mid];
+                if(temp >= (success / spell)) //find the most left one if the find vector has duplicated elements
                 {
-                    right = mid - 1;
+                    right = mid;
                 }
                 else
                 {
@@ -25,12 +24,12 @@ public:
                 }
             }
 
-            returnVec.push_back(potions.size() - left);
+            returnArr.push_back(potions.size() - left);
         }
-        
-        return returnVec;
+
+        return returnArr;
     }
 };
 
-//time complexity => sort: O(mlogm) + binary search: O(nlogm)
-//space complexity => O(n)
+//time complexity: sort O(mlogm) + binary search (nlogm)
+//space complexity: O(n)
