ffb3b184acb7bd572ba923c574696ada375d2e24
Kueiju <73866494+Kueiju@users.noreply.github.com>
2025-04-01 23:12:02 +0800

    Time: 48 ms (17.64%), Space: 128.3 MB (8.35%) - LeetHub

---
+++ 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
+
+        sort(potions.begin(), potions.end());
+
+        vector<int> returnVec;
+        
+        for(auto val : spells)
+        {
+            int left = 0;
+            int right = potions.size() - 1;
+
+            while(left <= right)
+            {
+                int mid = left + (right - left) / 2;
+                if(potions[mid] >= (success + val - 1) / val)
+                {
+                    right = mid - 1;
+                }
+                else
+                {
+                    left = mid + 1;
+                }
+            }
+
+            returnVec.push_back(potions.size() - left);
+        }
+        
+        return returnVec;
+    }
+};
+
+//time complexity => sort: O(mlogm) + binary search: O(nlogm)
+//space complexity => O(n)
