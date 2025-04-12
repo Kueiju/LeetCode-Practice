276067c7e91964b1e48b253c56f640c08a54f68b
Kueiju <73866494+Kueiju@users.noreply.github.com>
2025-04-12 14:26:51 +0800

    Time: 0 ms (100%), Space: 18.9 MB (75.59%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -16,9 +16,9 @@ public:
         if(root == nullptr)
             return 0;
         
-        int left = 1 + maxDepth(root->left);
-        int right = 1 + maxDepth(root->right);
+        int left = maxDepth(root->left);
+        int right = maxDepth(root->right);
 
-        return max(left, right);    
+        return max(left, right) + 1;    
     }
 };
