ae1e86338b4521cd3887039c96a6efa8f1241a33
Kueiju <73866494+Kueiju@users.noreply.github.com>
2025-08-13 21:27:40 +0800

    Time: 4 ms (68.91%), Space: 134.3 MB (83.03%) - LeetHub

--- 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.cpp
+++ 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.cpp
@@ -17,15 +17,22 @@ public:
         {
             sum += nums[i];
         }
-        
+         //ans[k] = sum;
         ans[k] = sum / (2*k + 1);
         
-        for(int i = k + 1; i < size - k; i++)
+        for(int i = 2*k + 1; i < size; i++)
         {
-            sum += nums[i + k] - nums[i - k - 1];
-            ans[i] = sum / (2*k + 1);
+            sum += nums[i] - nums[i - 2*k - 1];
+            ans[i - k] = sum / (2*k + 1);
+            //ans[i] = sum / (2*k + 1);
         }
         
+        // for(int i = 0; i < size; i++)
+        // {
+        //     if(ans[i] != -1)
+        //         ans[i] /= (2*k + 1);
+        // }
+
         return ans;
     }
 };
