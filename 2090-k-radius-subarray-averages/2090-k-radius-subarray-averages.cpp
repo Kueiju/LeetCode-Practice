b6ccb836a388a7d880b125e0ecf7ad1376b29118
Kueiju <73866494+Kueiju@users.noreply.github.com>
2025-08-13 19:47:02 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<int> getAverages(vector<int>& nums, int k) {
+        if(k == 0)
+            return nums;
+
+        int size = nums.size();
+        
+        vector<long long> ans (size, -1);
+        
+        if(size < 2*k + 1)
+            return ans;
+        
+        long long sum = 0;
+        
+        for(int i = 0; i < 2*k + 1; i++)
+        {
+            sum += nums[i];
+        }
+        
+        ans[k] = sum / (2*k + 1);
+        
+        for(int i = k + 1; i < size - k; i++)
+        {
+            sum += nums[i + k] - nums[i - k - 1];
+            ans[i] = sum / (2*k + 1);
+        }
+        
+        return ans;
+    }
+};
