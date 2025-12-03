e150c9acf61cd2dced3da37d184c85ed86ada191
Kueiju <73866494+Kueiju@users.noreply.github.com>
2025-12-03 23:47:27 +0800

    Time: 3 ms (17.61%), Space: 15.8 MB (6%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.cpp
+++ 0841-keys-and-rooms/0841-keys-and-rooms.cpp
@@ -1,30 +1,39 @@
 class Solution {
 public:
 
-    unordered_set<int> set;
+    vector<bool> seen;
     vector<vector<int>> rooms;
-
-    void dfs(int node)
-    {
-        for(auto val : rooms[node])
-        {
-            if(!set.contains(val))
-            {
-                set.insert(val);
-
-                dfs(val);
-            }
-        }
-    }
+    unordered_set<int> set;
 
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
+        
+        seen = vector(rooms.size(), false);
+        
         this->rooms = rooms;
 
+        seen[0] = true;
         set.insert(0);
-
         dfs(0);
 
-        return set.size() == rooms.size();
-        
+        for(int i = 0; i < rooms.size(); i++)
+        {
+            if(set.find(i) == set.end())
+                return false;
+        }
+
+        return true;
+    }
+    
+    void dfs(int node)
+    {
+        for(int neighbor : rooms[node])
+        {
+            if(!seen[neighbor])
+            {
+                seen[neighbor] = true;
+                set.insert(neighbor);
+                dfs(neighbor);
+            }
+        }
     }
 };
