09478da6d0f9390c62f90632f30e8244a56344f8
Kueiju <73866494+Kueiju@users.noreply.github.com>
2025-12-07 00:15:56 +0800

    Time: 95 ms (95.72%), Space: 145 MB (95.34%) - LeetHub

--- 2368-reachable-nodes-with-restrictions/2368-reachable-nodes-with-restrictions.cpp
+++ 2368-reachable-nodes-with-restrictions/2368-reachable-nodes-with-restrictions.cpp
@@ -1,46 +1,43 @@
+class UnionFind {
+    vector<int> root, rank;
+public:
+    UnionFind(int n) : root(n), rank(n, 1) {
+        iota(root.begin(), root.end(), 0);
+    }
+    int find(int x) {
+        if (x != root[x]) {
+            root[x] = find(root[x]);
+        }
+        return root[x];
+    }
+    void join(int x, int y) {
+        int rootX = find(x), rootY = find(y);
+        if (rootX != rootY) {
+            if (rank[rootX] > root[rootY]) {
+                swap(rootX, rootY);
+            }
+            root[rootX] = rootY;
+            rank[rootY] += rank[rootX];
+        }
+    }
+    int getSize(int x) {
+        return rank[find(x)];
+    }
+};
+
 class Solution {
 public:
-    
-    unordered_map<int, vector<int>> graph;
-    
-    vector<bool> seen;
-    
     int reachableNodes(int n, vector<vector<int>>& edges, vector<int>& restricted) {
-        
-        seen.assign(n, false);
-        
-        for(auto edge : edges)
-        {
-            int x = edge[0];
-            int y = edge[1];
-            
-            graph[x].push_back(y);
-            graph[y].push_back(x);
-        }
-        
-        //seen[0] = true;
-        
-        unordered_set<int> set (restricted.begin(), restricted.end());
-        
-        return dfs(0, set);
-    }
-    
-    int dfs(int node, unordered_set<int>& set)
-    {
-        seen[node] = true;
-        
-        int ans = 1;
-        
-        for(int neighbor : graph[node])
-        {
-            if(!seen[neighbor] && !set.contains(neighbor))
-            {
-                ans += dfs(neighbor, set);
+        UnionFind uf(n);
+        unordered_set<int> restSet(restricted.begin(), restricted.end());
+
+        for (auto edge : edges) {
+            int a = edge[0], b = edge[1];
+            if (restSet.find(a) == restSet.end() && restSet.find(b) == restSet.end()) {
+                uf.join(a, b);
             }
         }
         
-        return ans;
+        return uf.getSize(0);
     }
-    
-    
 };
